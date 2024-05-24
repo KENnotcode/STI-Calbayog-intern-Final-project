e3518b18d8c4b7a2c22500e0ca529a390c7eb017
ken <ccnhs_cubilo0098@calbayog.ph.education>
2024-05-24 10:48:23 +0800

    added input validateFor to validate if the user filled all the required fields. Else, it will return a notification saying Please fill out all required fields.

--- .next/static/webpack/webpack.08251ca39f2d1e4e.hot-update.js
+++ .next/static/webpack/webpack.08251ca39f2d1e4e.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ff6944b51f31b767"; }
+/******/ 	__webpack_require__.h = function() { return "40ec5eb2ba549718"; }
 /******/ }();
 /******/ 
 /******/ }
